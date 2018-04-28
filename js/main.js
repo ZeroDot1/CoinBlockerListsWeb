5a4ac88581daae5ab1c859deca0b2ccb7a29eac7
ZeroDot1 <zerodot1@bk.ru>
2018-04-29 01:06:40 +0200

    Update . . .

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert("Hello CoinBlockerLists User!\n!!!Warning!!! This page is still under construction!!!");
+//alert("Hello CoinBlockerLists User!\n!!!Warning!!! This page is still under construction!!!");
+
+function openColorBox(){
+        $.colorbox({
+          iframe:true,
+          width:"50%",
+          height:"50%",
+          href: "donate.html",
+          onLoad: function() {
+            $('#cboxClose').remove();
+            setTimeout(function(){
+              $(window).colorbox.close();
+            }, 9000)
+          }
+        });
+      }
+
+      function countDown(){
+        seconds--
+        $("#seconds").text(seconds);
+        if (seconds === 0){
+          openColorBox();
+          clearInterval(i);
+        }
+      }
+
+      var seconds = 5,
+          i = setInterval(countDown, 1000);
