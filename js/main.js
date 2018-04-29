939c420ef69e7cdd7df5a7b3b3692ba7de41912d
ZeroDot1 <zerodot1@bk.ru>
2018-04-29 21:52:13 +0200

    Upgrade . . .

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //alert("Hello CoinBlockerLists User!\n!!!Warning!!! This page is still under construction!!!");
 
 //https://getbootstrap.com/docs/3.3/customize/
-window.onload = function() {
-  document.getElementById('autoload').click()
-}
+//window.onload = function() {
+//document.getElementById('autoload').click()
+//}
